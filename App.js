0330410323bb5030ebb5c8f3178fd4a19c00ecc5
08jhs05 <08jhs05@gmail.com>
2021-06-26 18:03:49 -0400

    load fonts from App.js and change state when the font is ready

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { Home, Restaurant, OrderDelivery } from "./screens";
 
+import AppLoading from 'expo-app-loading';
+
 import Tabs from "./navigation/tabs"
+import * as Font from "expo-font";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  let customFonts = {
+    'SFProRounded-Regular': require('./assets/fonts/SFProRounded-Regular.ttf'),
+    'SFProRounded-Bold': require('./assets/fonts/SFProRounded-Bold.ttf')
+  };
+
+  const loadFontsAsync = async function() {
+      await Font.loadAsync(customFonts);
+      setFontLoaded(true);
+  };
+
+  useEffect( () => {
+    loadFontsAsync();
+  })
+
   return (
-    <NavigationContainer>
+    fontLoaded ? <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
           headerShown: false
@@ -20,6 +40,6 @@ export default function App() {
         <Stack.Screen name="Restaurant" component={Restaurant}/>
         <Stack.Screen name="OrderDelivery" component={OrderDelivery}/>
       </Stack.Navigator>
-    </NavigationContainer>
+    </NavigationContainer> : <AppLoading />
   );
 }
