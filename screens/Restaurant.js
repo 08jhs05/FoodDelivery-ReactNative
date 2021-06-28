19bb5523390673826428ea1aeb0d9ac0df372ab2
08jhs05 <08jhs05@gmail.com>
2021-06-28 01:19:26 -0400

    add bottom order view

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -26,6 +26,12 @@ const Restaurant = ({ route, navigation }) => {
     setcurrentLocation(currentLocation)
   });
 
+  function getBasketItemCount() {
+    let itemCount = orderItems.reduce((a, b) => a + (b.qty || 0), 0)
+
+    return itemCount
+}
+
   function getOrderQty(menuId) {
     let orderItem = orderItems.filter(a => a.menuId == menuId)
 
@@ -34,6 +40,11 @@ const Restaurant = ({ route, navigation }) => {
     }
 
     return 0
+}
+function sumOrder() {
+  let total = orderItems.reduce((a, b) => a + (b.total || 0), 0)
+
+  return total.toFixed(2)
 }
 
   function editOrder(action, menuId, price) {
@@ -249,10 +260,169 @@ const Restaurant = ({ route, navigation }) => {
     )
 }
 
+function renderDots() {
+
+  const dotPosition = Animated.divide(scrollX, SIZES.width)
+
+  return (
+      <View style={{ height: 30 }}>
+          <View
+              style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  height: SIZES.padding
+              }}
+          >
+              {restaurant?.menu.map((item, index) => {
+
+                  const opacity = dotPosition.interpolate({
+                      inputRange: [index - 1, index, index + 1],
+                      outputRange: [0.3, 1, 0.3],
+                      extrapolate: "clamp"
+                  })
+
+                  const dotSize = dotPosition.interpolate({
+                      inputRange: [index - 1, index, index + 1],
+                      outputRange: [SIZES.base * 0.8, 10, SIZES.base * 0.8],
+                      extrapolate: "clamp"
+                  })
+
+                  const dotColor = dotPosition.interpolate({
+                      inputRange: [index - 1, index, index + 1],
+                      outputRange: [COLORS.darkgray, COLORS.primary, COLORS.darkgray],
+                      extrapolate: "clamp"
+                  })
+
+                  return (
+                      <Animated.View
+                          key={`dot-${index}`}
+                          opacity={opacity}
+                          style={{
+                              borderRadius: SIZES.radius,
+                              marginHorizontal: 6,
+                              width: dotSize,
+                              height: dotSize,
+                              backgroundColor: dotColor
+                          }}
+                      />
+                  )
+              })}
+          </View>
+      </View>
+  )
+}
+
+function renderOrder() {
+  return (
+      <View>
+          {
+              renderDots()
+          }
+          <View
+              style={{
+                  backgroundColor: COLORS.white,
+                  borderTopLeftRadius: 40,
+                  borderTopRightRadius: 40
+              }}
+          >
+              <View
+                  style={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      paddingVertical: SIZES.padding * 2,
+                      paddingHorizontal: SIZES.padding * 3,
+                      borderBottomColor: COLORS.lightGray2,
+                      borderBottomWidth: 1
+                  }}
+              >
+                  <Text style={{ ...FONTS.h3 }}>{getBasketItemCount()} items in Cart</Text>
+                  <Text style={{ ...FONTS.h3 }}>${sumOrder()}</Text>
+              </View>
+
+              <View
+                  style={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      paddingVertical: SIZES.padding * 2,
+                      paddingHorizontal: SIZES.padding * 3
+                  }}
+              >
+                  <View style={{ flexDirection: 'row' }}>
+                      <Image
+                          source={icons.pin}
+                          resizeMode="contain"
+                          style={{
+                              width: 20,
+                              height: 20,
+                              tintColor: COLORS.darkgray
+                          }}
+                      />
+                      <Text style={{ marginLeft: SIZES.padding, ...FONTS.h4 }}>Location</Text>
+                  </View>
+
+                  <View style={{ flexDirection: 'row' }}>
+                      <Image
+                          source={icons.master_card}
+                          resizeMode="contain"
+                          style={{
+                              width: 20,
+                              height: 20,
+                              tintColor: COLORS.darkgray
+                          }}
+                      />
+                      <Text style={{ marginLeft: SIZES.padding, ...FONTS.h4 }}>8888</Text>
+                  </View>
+              </View>
+
+              {/* Order Button */}
+              <View
+                  style={{
+                      padding: SIZES.padding * 2,
+                      alignItems: 'center',
+                      justifyContent: 'center'
+                  }}
+              >
+                  <TouchableOpacity
+                      style={{
+                          width: SIZES.width * 0.9,
+                          padding: SIZES.padding,
+                          backgroundColor: COLORS.primary,
+                          alignItems: 'center',
+                          borderRadius: SIZES.radius
+                      }}
+                      onPress={() => navigation.navigate("OrderDelivery", {
+                          restaurant: restaurant,
+                          currentLocation: currentLocation
+                      })}
+                  >
+                      <Text style={{ color: COLORS.white, ...FONTS.h2 }}>Order</Text>
+                  </TouchableOpacity>
+              </View>
+          </View>
+
+          {isIphoneX() &&
+              <View
+                  style={{
+                      position: 'absolute',
+                      bottom: -34,
+                      left: 0,
+                      right: 0,
+                      height: 34,
+                      backgroundColor: COLORS.white
+                  }}
+              >
+              </View>
+          }
+      </View>
+  )
+}
+
   return (
     <SafeAreaView style={styles.container}>
       {renderHeader()}
       {renderFoodInfo()}
+      {renderOrder()}
     </SafeAreaView>
   )
 
