6443baa3cc70a85a51359fe50141ad32f8722ab1
08jhs05 <08jhs05@gmail.com>
2021-06-28 00:54:26 -0400

    update restaurant screen to show their menu

--- screens/Home.js
+++ screens/Home.js
@@ -10,7 +10,7 @@ import {
 } from "react-native";
 import { COLORS, icons, images, SIZES, FONTS } from "../constants";
 
-const Home = () => {
+const Home = ({ navigation }) => {
 
   // ================================= DUMMY DATA =================================
 
